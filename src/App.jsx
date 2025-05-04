f165ae36d7d6e1e2e59f6500a187010e77d27235
ayush <ayushchouhan417@gmail.com>
2025-05-04 19:29:35 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter basename="/FinanceHeader">
     <Nav/>
     <Routes>
        <Route path="/" element={<Home />}/>
